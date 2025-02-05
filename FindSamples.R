5e002946071fc3680a8cc8f09bb2819c4d4f4725
MVesuviusC <matthewvc1@gmail.com>
2025-02-05 15:31:31 -0500

    Commit with Ryan's changes befor further updates

--- FindSamples.R
+++ FindSamples.R
@@ -1,55 +1,68 @@
 library(tidyverse)
 library(Seurat)
 library(rrrSingleCellUtils)
+library(future)
+library(future.apply)
+if(Sys.info()['sysname'] == "Windows") {
+  plan(multisession)
+} else {
+  plan(multicore)
+}
 
 samples <- c(
-  "S0007",
-#  "S0008",
-#  "S0009",
-  "S0015",
-  "S0016",
-  "S0023",
-  "S0024",
-  "S0027",
-#  "S0065",
-#  "S0073",
-  "S0082",
-  "S0084",
-  "S0086",
-  "S0092",
-  "S0093",
-  "S0097",
-#  "S0098",
-#  "S0099",
-  "S0124",
-#  "S0125",
-  "S0132",
-  "S0279",
-  "S0280",
-  "S0281"
+  # "S0007",
+  # "S0015",
+  # "S0016",
+  # "S0023",
+  # "S0024",
+  # "S0027",
+  # "S0082",
+  # "S0084",
+  # "S0086",
+  # "S0092",
+  # "S0093",
+  # "S0097",
+  # "S0124",
+  # "S0132",
+  # "S0279",
+  # "S0280",
+  # "S0281"
+  "S0018",
+  "S0037",
+  "S0048",
+  "S0131"
 )
-pre_path <- "/gpfs0/home2/gdrobertslab/lab/Counts_2/"
+# pre_path <- "/gpfs0/home2/gdrobertslab/lab/Counts_2/"
+pre_path <- "C:/Users/rxr014/OneDrive - Nationwide Children's Hospital/BIScratch/"
 post_path <- "/filtered_feature_bc_matrix"
 
 
-OS17 <- parallel::mclapply(samples, function(s) { # nolint
-  tenx_load_qc(paste0(pre_path, s, post_path),
+OS17 <- future_lapply(samples, function(s) { # nolint
+  warning(paste("Starting sample ", s, "..."))
+  obj <- tenx_load_qc(paste0(pre_path, s, post_path),
     species_pattern = "^GRCh38-",
-    violin_plot = FALSE) %>%
-  NormalizeData() %>%
+    violin_plot = FALSE)
+
+  if(length(Cells(obj)) > 1500) {
+    subset(obj, cells = sample(Cells(obj), 1500))
+  }
+
+  obj <- NormalizeData(obj) %>%
   FindVariableFeatures() %>%
   ScaleData() %>%
   RunPCA() %>%
   RunUMAP(reduction = "pca", dims = 1:30)
-}, mc.cores = parallelly::availableCores())
+
+  warning(paste("Sample ", s, " completed."))
+  return(obj)
+})
 
 names(OS17) <- samples
 
-plots <- parallel::mclapply(seq_along(OS17), function(i) {
-  p <- r_feature_plot(OS17[[i]], "CXCL8")
+plots <- lapply(seq_along(OS17), function(i) {
+  p <- r_feature_plot(OS17[[i]], "CXCL8") +
+    labs(title = paste("CXCL8 - ", names(OS17)[i]))
   return(p)
-}, mc.cores = parallelly::availableCores())
+})
 
-plots
-plots[[14]]
-samples[14]
+patchwork::wrap_plots(plots, ncol = 4)
